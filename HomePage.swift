f36b04c80c1498fa5ede236fa48aa7069fb09f0d
lydiaostos <ostoslydia@gmail.com>
2025-07-26 16:30:18 -0700

    Add files via upload

---
+++ HomePage.swift
@@ -0,0 +1,49 @@
+//
+//  HomePage.swift
+//  test
+//
+//  Created by Lydia Ostos on 7/21/25.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    @State private var selectedTab: appTab = .home
+    @State private var tutorialPageIndex: Int = 0
+
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            MainView(selectedTab: $selectedTab)
+                .tag(appTab.home)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            tutorialPage(
+                selectedTab: $selectedTab,
+                tutorialPageIndex: $tutorialPageIndex
+            )
+            .tag(appTab.tutorial)
+            .tabItem {
+                Label("Tutorial", systemImage: "figure")
+            }
+
+            PeopleView()
+                .tag(appTab.faces)
+                .tabItem {
+                    Label("My People", systemImage: "person.2")
+                }
+        }
+        .onChange(of: selectedTab) {
+            if selectedTab == .tutorial {
+                tutorialPageIndex = 0
+            }
+        }
+    }
+}
+
+#Preview {
+    HomePage()
+}
+
