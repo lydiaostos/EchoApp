f50ba4eb38a1b10f4dfaf5be3baba27f5b7d59f6
lydiaostos <ostoslydia@gmail.com>
2025-07-26 16:40:47 -0700

    Update HapticStrength.swift

--- HapticStrength.swift
+++ HapticStrength.swift
@@ -1,12 +1,1 @@
-//
-//  HapticStrength.swift
-//  test
-//
-//  Created by Lydia Ostos on 7/22/25.
-//
 
-import Foundation
-enum HapticStrength: String, CaseIterable, Identifiable {
-    case low, medium, high
-    var id: String { rawValue }
-}
